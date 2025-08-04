8ad723dd41168229804669e733f884be68fe4120
Rami Reddy <ramyk@hotmail.com>
2025-08-03 22:14:21 -0400

    vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,8 +8,23 @@ export default defineConfig({
     proxy: {
       "/api": "http://localhost:5000",
     },
-    build: {
-      chunkSizeWarningLimit: 450, // Set your desired limit in KB (e.g., 1000 for 1MB)
+    // build: {
+    //   chunkSizeWarningLimit: 450, // Set your desired limit in KB (e.g., 1000 for 1MB)
+    // },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString();
+          }
+        },
+      },
     },
   },
 });
