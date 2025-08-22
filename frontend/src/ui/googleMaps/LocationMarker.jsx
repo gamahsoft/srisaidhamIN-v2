704dd80c8acb8db0766e6fbd7358d21a894be03f
Rami Reddy <ramyk@hotmail.com>
2025-08-21 22:43:20 -0400

    made changes to LocationMarker.jsx

--- frontend/src/ui/googleMaps/LocationMarker.jsx
+++ frontend/src/ui/googleMaps/LocationMarker.jsx
@@ -1,19 +1,42 @@
-import React from "react";
+// import React from "react";
 // import { Icon } from "@iconify/react";
-import locationTemple from "@iconify/icons-mdi/hinduism";
-import locationRestaurant from "@iconify/icons-mdi/local-restaurant";
-// import locationGrocery from '@iconify/icons-mdi/local-grocery-store'
-// import locationGrocery from '@iconify/icons-mdi/local-restaurant'
+// import locationTemple from "@iconify/icons-mdi/hinduism";
+// import locationRestaurant from "@iconify/icons-mdi/local-restaurant";
+// // import locationGrocery from '@iconify/icons-mdi/local-grocery-store'
+// // import locationGrocery from '@iconify/icons-mdi/local-restaurant'
+// import "./maps.css";
+
+// const LocationMarker = ({ lat, lng, type, onClick }) => {
+//   return (
+//     <div className="location-marker" onClick={onClick}>
+//       {type === "Mandhir" ? (
+//         <Icon className="location-icon" icon={locationTemple} />
+//       ) : (
+//         <Icon className="location-icon" icon={locationRestaurant} />
+//       )}
+//     </div>
+//   );
+// };
+
+// export default LocationMarker;
+
+import React from "react";
+import { Icon } from "@iconify/react";
+import hinduism from "@iconify-icons/mdi/hinduism";
+import localRestaurant from "@iconify-icons/mdi/local-restaurant";
 import "./maps.css";
 
 const LocationMarker = ({ lat, lng, type, onClick }) => {
+  const iconData = type === "Mandhir" ? hinduism : localRestaurant;
+
   return (
-    <div className="location-marker" onClick={onClick}>
-      {/* {type === "Mandhir" ? (
-        <Icon className="location-icon" icon={locationTemple} />
-      ) : (
-        <Icon className="location-icon" icon={locationRestaurant} />
-      )} */}
+    <div
+      className="location-marker"
+      onClick={onClick}
+      role="button"
+      aria-label={type}
+    >
+      <Icon className="location-icon" icon={iconData} />
     </div>
   );
 };
