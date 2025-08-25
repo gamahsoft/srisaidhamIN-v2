5eaa18490a693d4288f76498e5e6ce8f55dc18f1
Rami Reddy <ramyk@hotmail.com>
2025-08-24 22:43:49 -0400

    CheckoutForm.jsx stripe payment

--- frontend/src/pages/CheckoutForm.jsx
+++ frontend/src/pages/CheckoutForm.jsx
@@ -40,8 +40,10 @@ export default function CheckoutForm() {
 
     setIsLoading(true);
 
-    const returnUrl = new URL("payment/PaymentSuccess", window.location.origin)
-      .href;
+    // const returnUrl = new URL("payment/PaymentSuccess", window.location.origin)
+    const returnUrl = new URL(
+      "https://srisaidhamin-v2-1.onrender.com/paymentSuccess"
+    );
 
     const { error, paymentIntent } = await stripe.confirmPayment({
       elements,
