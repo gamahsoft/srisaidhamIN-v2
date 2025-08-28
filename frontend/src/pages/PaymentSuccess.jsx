4e399a715d541784eb578e7dfbb6b995f1a09277
Rami Reddy <ramyk@hotmail.com>
2025-08-27 23:07:29 -0400

    PaymentSuccess.jsx changes

--- frontend/src/pages/PaymentSuccess.jsx
+++ frontend/src/pages/PaymentSuccess.jsx
@@ -167,7 +167,6 @@ function PaymentSuccess() {
         case "processing":
           // Payment is still processing
           toast.success("Payment is still processing 😎");
-          console.log("Your payment is processing.");
           break;
         case "requires_payment_method":
           // Payment failed
@@ -182,39 +181,39 @@ function PaymentSuccess() {
   }, [stripe, location, dispatch]);
 
   // Create order information
-  // useEffect(() => {
-  //   if (hasRun.current) return;
+  useEffect(() => {
+    if (hasRun.current) return;
 
-  //   hasRun.current = true;
-  //   // Define async function
-  //   const placeOrderHandler = async () => {
-  //     try {
-  //       const res = await createOrder({
-  //         orderItems: cart.cartItems,
-  //         // shippingAddress: cart.shippingAddress,
-  //         paymentMethod: cart.paymentMethod,
-  //         itemsPrice: cart.itemsPrice,
-  //         // shippingPrice: cart.shippingPrice,
-  //         // taxPrice: cart.taxPrice,
-  //         totalPrice: cart.totalPrice,
-  //       }).unwrap();
-  //       dispatch(clearCartItems());
-  //       toast.success("Donation record successfully created 😎");
-  //     } catch (err) {
-  //       toast.error(err?.data?.message || "Order failed");
-  //     }
-  //   };
+    hasRun.current = true;
+    // Define async function
+    const placeOrderHandler = async () => {
+      try {
+        const res = await createOrder({
+          orderItems: cart.cartItems,
+          // shippingAddress: cart.shippingAddress,
+          paymentMethod: cart.paymentMethod,
+          itemsPrice: cart.itemsPrice,
+          // shippingPrice: cart.shippingPrice,
+          // taxPrice: cart.taxPrice,
+          totalPrice: cart.totalPrice,
+        }).unwrap();
+        dispatch(clearCartItems());
+        toast.success("Donation record successfully created 😎");
+      } catch (err) {
+        toast.error(err?.data?.message || "Order failed");
+      }
+    };
 
-  //   // call the async function
-  //   placeOrderHandler();
-  // }, [
-  //   cart.cartItems,
-  //   cart.itemsPrice,
-  //   cart.totalPrice,
-  //   createOrder,
-  //   cart.paymentMethod,
-  //   dispatch,
-  // ]); // empty dependency array = run once on mount
+    // call the async function
+    placeOrderHandler();
+  }, [
+    cart.cartItems,
+    cart.itemsPrice,
+    cart.totalPrice,
+    createOrder,
+    cart.paymentMethod,
+    dispatch,
+  ]); // empty dependency array = run once on mount
 
   if (isLoading)
     return (
