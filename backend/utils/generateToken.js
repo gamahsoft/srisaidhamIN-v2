74126dd9d105cf231a81878ab162f2d0b6963e19
Rami Reddy <ramyk@hotmail.com>
2025-09-04 22:33:59 -0400

    updated generateToken samesite:

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -9,7 +9,8 @@ const generateToken = (res, userId) => {
   res.cookie("jwt", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV !== "development", // Use secure cookies in production
-    sameSite: "strict", // Prevent CSRF attacks
+    // sameSite: "strict", // Prevent CSRF attacks
+    sameSite: "none",
     maxAge: 90 * 24 * 60 * 60 * 1000, //90 days
   });
 };
