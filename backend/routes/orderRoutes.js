5d106148e6b41964b9b435c251f029da0bd03784
Rami Reddy <ramyk@hotmail.com>
2025-09-10 23:00:35 -0400

    backend myroders changes to orderRoutes.js

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -20,7 +20,7 @@ router.route("/").post(protect, addOrderItems).get(protect, admin, getOrders);
 // create stripe payment intent
 router.post("/payment-intent", createPaymentIntent);
 // router.route("/myorders").get(protect, getMyOrders);
-router.route("/myorders").get(getMyOrders);
+router.get("/myorders", getMyOrders);
 //make sure this order by id route is below the '/' route but not before
 router.route("/:id").get(protect, getOrderById);
 router.route("/:id/pay").put(protect, updateOrderToPaid);
