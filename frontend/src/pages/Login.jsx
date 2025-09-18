262fc9941975b42832bef44ed3e88b2668a0f4ae
Rami Reddy <ramyk@hotmail.com>
2025-09-17 21:52:31 -0400

    login.jsx changes to add guest user functionality

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -198,32 +198,34 @@ function Login() {
                   <SpinnerMini />
                 ) : (
                   <>
-                    <button
-                      type="submit"
-                      disabled={isLoading}
-                      className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-slate-700 hover:bg-slate-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                    >
-                      <span className="absolute left-0 inset-y-0 flex items-center pl-3">
-                        <LockClosedIcon
-                          className="h-5 w-5 text-orange-400 group-hover:text-orange-400"
-                          aria-hidden="true"
-                        />
-                      </span>
-                      Login
-                    </button>
-                    <button
-                      type="submit"
-                      disabled={isLoading}
-                      className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-slate-700 hover:bg-slate-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                    >
-                      <span className="absolute left-0 inset-y-0 flex items-center pl-3">
-                        <LockClosedIcon
-                          className="h-5 w-5 text-orange-400 group-hover:text-orange-400"
-                          aria-hidden="true"
-                        />
-                      </span>
-                      Login
-                    </button>
+                    <div className="flex justify-center">
+                      <button
+                        type="submit"
+                        disabled={isLoading}
+                        className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-slate-700 hover:bg-slate-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                      >
+                        <span className="absolute left-0 inset-y-0 flex items-center pl-3">
+                          <LockClosedIcon
+                            className="h-5 w-5 text-orange-400 group-hover:text-orange-400"
+                            aria-hidden="true"
+                          />
+                        </span>
+                        Login
+                      </button>
+                      <button
+                        type="submit"
+                        disabled={isLoading}
+                        className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-slate-700 hover:bg-slate-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                      >
+                        <span className="absolute left-0 inset-y-0 flex items-center pl-3">
+                          <LockClosedIcon
+                            className="h-5 w-5 text-orange-400 group-hover:text-orange-400"
+                            aria-hidden="true"
+                          />
+                        </span>
+                        Guest User
+                      </button>
+                    </div>
                   </>
                 )}
 
