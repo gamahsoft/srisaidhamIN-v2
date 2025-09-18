b5b6dc96466c7c4ee31ad6dc914f10d1c5852484
Rami Reddy <ramyk@hotmail.com>
2025-09-17 22:57:19 -0400

    login.js user guest access changes

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -222,7 +222,7 @@ function Login() {
                       </button>
                       <button
                         onSubmit={guestSubmit}
-                        type="submit"
+                        type="button"
                         disabled={isLoading}
                         className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-slate-700 hover:bg-slate-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                       >
