4e331fa7440f30d122fbb85d4556f16fe27f6b54
Rami Reddy <ramyk@hotmail.com>
2025-09-18 22:41:38 -0400

    login.jsx changes for guest user

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -65,14 +65,10 @@ function Login() {
     }
   }
 
-  function guestSubmit() {
-    try {
-      alert("Email is valid!");
-      navigate(redirect);
-    } catch (err) {
-      toast.error("err?.data?.message" || err.error);
-    }
-  }
+  const guestSubmit = () => {
+    alert("You are in gust submit!");
+    navigate(redirect);
+  };
 
   // const backgroundImageStyle = {
   //   backgroundImage: `url("${loginbg}")`,
@@ -207,35 +203,33 @@ function Login() {
                   <SpinnerMini />
                 ) : (
                   <>
-                    <div className="flex justify-center">
-                      <button
-                        type="submit"
-                        disabled={isLoading}
-                        className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-slate-700 hover:bg-slate-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                      >
-                        <span className="absolute left-0 inset-y-0 flex items-center pl-3 pr-3">
-                          <LockClosedIcon
-                            className="h-5 w-5 text-orange-400 group-hover:text-orange-400"
-                            aria-hidden="true"
-                          />
-                        </span>
-                        Login
-                      </button>
-                      <button
-                        onSubmit={guestSubmit}
-                        type="button"
-                        disabled={isLoading}
-                        className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-slate-700 hover:bg-slate-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                      >
-                        {/* <span className="absolute left-0 inset-y-0 flex items-center pl-3">
-                          <LockClosedIcon
-                            className="h-5 w-5 text-orange-400 group-hover:text-orange-400"
-                            aria-hidden="true"
-                          />
-                        </span> */}
-                        Guest User
-                      </button>
-                    </div>
+                    <button
+                      type="submit"
+                      disabled={isLoading}
+                      className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-slate-700 hover:bg-slate-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                    >
+                      <span className="absolute left-0 inset-y-0 flex items-center pl-3 pr-3">
+                        <LockClosedIcon
+                          className="h-5 w-5 text-orange-400 group-hover:text-orange-400"
+                          aria-hidden="true"
+                        />
+                      </span>
+                      Login
+                    </button>
+                    <button
+                      onClick={guestSubmit}
+                      type="button"
+                      disabled={isLoading}
+                      className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-slate-700 hover:bg-slate-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                    >
+                      <span className="absolute left-0 inset-y-0 flex items-center pl-3">
+                        <LockClosedIcon
+                          className="h-5 w-5 text-orange-400 group-hover:text-orange-400"
+                          aria-hidden="true"
+                        />
+                      </span>
+                      Guest User
+                    </button>
                   </>
                 )}
 
